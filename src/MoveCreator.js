26948bd4afced7683cbcdc3dbf5644086daa60ed
nataleechu <nataleechu@gmail.com>
2025-03-27 14:26:14 -0500

    MoveMaker Page

--- src/MoveCreator.js
+++ src/MoveCreator.js
@@ -1,23 +1,52 @@
 import './MoveCreator.css';
 
-function UploadSkeleton() {
+function MoveCreator() {
 	return (
 		<>
-		<div className="Title">
-			<h1 class="TitleText">Move Creator</h1>
+		<div className="MTitle">
+			<h1 class="MTitleText">Move Creator</h1>
 		</div>
-    <div className="buttonandimg">
-        <div className="ButtonContainer">
-            <button className="Buttons">Browse</button>
-            <button className="Buttons">Dance Bank</button>
-            <button className="Buttons">Move Bank</button>
+    <div className="MPageContent">
+        <div className="MUploadVideoTitleContainer">
+            <h2 class="MBoldText">Title</h2>
+            <input type="text" id="title" name="title" placeholder='My Dance' class="MDanceInput"></input>
+            <div class="MUploadVideoContainer">
+                <h2 class="MRegText">Upload Video</h2>
+                <h3 class="MSmallText">Upload a video to convert into a dance. 
+                    Be sure the dancer's full body is in frame.
+                </h3>
+                <div class="MImportFileContainer"
+                onDragOver={(e) => e.preventDefault()} 
+                onDrop={(e) => {
+                    e.preventDefault();
+                    const file = e.dataTransfer.files[0];
+                    if (file && file.type.startsWith('video/')) {
+                        console.log('File dropped:', file);
+                    } else {
+                        alert('Please drop a valid video file.');
+                    }
+                }}>
+                    <h2 class="MDragText">Drag and drop video here</h2>
+                    <h2 class="MOrText">OR</h2>
+                    <label for="file">Browse Files</label>
+                    <input class="MChooseFile" type="file" id="file" name="file" accept="video/*" ></input>
+                </div>
+            </div>
         </div>
-        <div className="homeImage">
-            <img src="nothing.jpg" alt="homepage image" width="500" height="500"/>
+        <div className = "MEditSaveContainer">
+            <div class="saveButtons">
+                <button class="MSaveButton">Save</button>
+            </div>
+            <div class="MVideoPreviewContainer">
+                <h2 class="MRegText">Edit Movements</h2>
+                <h2 class="MSmallText">The movements from your video will be applied to this skeleton. Edit a frame by dragging motion points.</h2>
+                {/* <video controls width="250" height="250"></video> */}
+            </div>
+
         </div>
     </div>
 		</>
 	);
 }
 
-export default HomeScreen
+export default MoveCreator
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import HomeScreen from './HomeScreen';
+import MoveCreator from './MoveCreator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,7 @@ root.render(
 			<link href="https://fonts.googleapis.com/css2?family=Faustina:ital,wght@0,300..800;1,300..800&family=PT+Sans+Caption:wght@400;700&display=swap" rel="stylesheet"></link>
 		</head>
     <HomeScreen />
+    <MoveCreator />
   </React.StrictMode>
 );
 
