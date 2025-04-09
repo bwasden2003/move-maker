71820114c1b6d3352dfe386f96798e91cd720d8e
DLevi013 <danielsujaylevi@utexas.edu>
2025-04-09 14:11:45 -0500

    edited dance bank

---
+++ src/DanceHolder.js
@@ -0,0 +1,377 @@
+
+    const dances = 
+    [
+            {
+            title: "Poker Face",
+            artist: "Lady Gaga",
+            difficulty: "Medium",
+            time: "3:07",
+            tags: ["Pop", "Dance", "2000s"],
+            img: "./DancingDude.png"
+            },
+            {
+            title: "Moves Like Jagger",
+            artist: "Maroon 5",
+            difficulty: "Hard",
+            time: "4:39",
+            tags: ["Pop", "Dance", "2010s"],
+            img: "./DancingDude.png"
+            },
+            {
+            title: "Firework",
+            artist: "Katy Perry",
+            difficulty: "Medium",
+            time: "3:53",
+            tags: ["Pop", "Inspirational", "2010s"],
+            img: "./DancingDude.png"
+            },
+            {
+            title: "Just Dance",
+            artist: "Lady Gaga",
+            difficulty: "Medium",
+            time: "4:07",
+            tags: ["Pop", "Dance", "2000s"],
+            img: "./DancingDude.png"
+            },
+            {
+            title: "POP/STARS",
+            artist: "K/DA",
+            difficulty: "Hard",
+            time: "3:23",
+            tags: ["K-Pop", "Virtual", "2010s"],
+            img: "./DancingDude.png"
+            },
+            {
+            title: "U Can’t Touch This",
+            artist: "MC Hammer",
+            difficulty: "Medium",
+            time: "4:34",
+            tags: ["Hip-Hop", "Funk", "1990s"],
+            img: "./DancingDude.png"
+            },
+            {
+              title: "Blinding Lights",
+              artist: "The Weeknd",
+              difficulty: "Medium",
+              time: "3:20",
+              tags: ["Pop", "Synthwave", "2020s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Levitating",
+              artist: "Dua Lipa",
+              difficulty: "Medium",
+              time: "3:23",
+              tags: ["Pop", "Dance", "2020s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Uptown Funk",
+              artist: "Mark Ronson ft. Bruno Mars",
+              difficulty: "Hard",
+              time: "4:30",
+              tags: ["Funk", "Pop", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Shake It Off",
+              artist: "Taylor Swift",
+              difficulty: "Easy",
+              time: "3:39",
+              tags: ["Pop", "Dance", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Bad Guy",
+              artist: "Billie Eilish",
+              difficulty: "Medium",
+              time: "3:14",
+              tags: ["Pop", "Alternative", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Can’t Stop the Feeling!",
+              artist: "Justin Timberlake",
+              difficulty: "Easy",
+              time: "3:56",
+              tags: ["Pop", "Dance", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Hey Ya!",
+              artist: "OutKast",
+              difficulty: "Hard",
+              time: "3:55",
+              tags: ["Hip-Hop", "Funk", "2000s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Happy",
+              artist: "Pharrell Williams",
+              difficulty: "Easy",
+              time: "3:53",
+              tags: ["Pop", "Soul", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Old Town Road",
+              artist: "Lil Nas X",
+              difficulty: "Medium",
+              time: "2:37",
+              tags: ["Country", "Hip-Hop", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Don’t Start Now",
+              artist: "Dua Lipa",
+              difficulty: "Medium",
+              time: "3:03",
+              tags: ["Pop", "Disco", "2020s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Roar",
+              artist: "Katy Perry",
+              difficulty: "Easy",
+              time: "3:43",
+              tags: ["Pop", "Inspirational", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Señorita",
+              artist: "Shawn Mendes & Camila Cabello",
+              difficulty: "Medium",
+              time: "3:11",
+              tags: ["Pop", "Latin", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Timber",
+              artist: "Pitbull ft. Kesha",
+              difficulty: "Medium",
+              time: "3:24",
+              tags: ["Pop", "Dance", "Country"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Sorry",
+              artist: "Justin Bieber",
+              difficulty: "Medium",
+              time: "3:20",
+              tags: ["Pop", "Dancehall", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Party Rock Anthem",
+              artist: "LMFAO",
+              difficulty: "Hard",
+              time: "4:23",
+              tags: ["Dance", "Electronic", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Sugar",
+              artist: "Maroon 5",
+              difficulty: "Easy",
+              time: "3:55",
+              tags: ["Pop", "Soul", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Hips Don’t Lie",
+              artist: "Shakira ft. Wyclef Jean",
+              difficulty: "Medium",
+              time: "3:38",
+              tags: ["Latin", "Pop", "2000s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Counting Stars",
+              artist: "OneRepublic",
+              difficulty: "Medium",
+              time: "4:17",
+              tags: ["Pop", "Rock", "2010s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Take On Me",
+              artist: "a-ha",
+              difficulty: "Hard",
+              time: "3:46",
+              tags: ["Pop", "Synthpop", "1980s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Thriller",
+              artist: "Michael Jackson",
+              difficulty: "Hard",
+              time: "5:57",
+              tags: ["Pop", "Funk", "1980s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Beat It",
+              artist: "Michael Jackson",
+              difficulty: "Medium",
+              time: "4:18",
+              tags: ["Rock", "Pop", "1980s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Material Girl",
+              artist: "Madonna",
+              difficulty: "Medium",
+              time: "4:00",
+              tags: ["Pop", "Dance", "1980s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Footloose",
+              artist: "Kenny Loggins",
+              difficulty: "Medium",
+              time: "3:45",
+              tags: ["Rock", "Dance", "1980s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Toxic",
+              artist: "Britney Spears",
+              difficulty: "Hard",
+              time: "3:19",
+              tags: ["Pop", "Dance", "2000s"],
+              img: "./DancingDude.png"
+            },
+            {
+              title: "Boom Boom Pow",
+              artist: "Black Eyed Peas",
+              difficulty: "Hard",
+              time: "4:11",
+              tags: ["Hip-Hop", "Electronic", "2000s"],
+              img: "./DancingDude.png"
+            },
+                {
+                  title: "Dynamite",
+                  artist: "BTS",
+                  difficulty: "Medium",
+                  time: "3:19",
+                  tags: ["K-Pop", "Dance", "2020s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "As It Was",
+                  artist: "Harry Styles",
+                  difficulty: "Medium",
+                  time: "2:47",
+                  tags: ["Pop", "Indie", "2020s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Heat Waves",
+                  artist: "Glass Animals",
+                  difficulty: "Medium",
+                  time: "3:58",
+                  tags: ["Alternative", "Pop", "2020s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Mr. Brightside",
+                  artist: "The Killers",
+                  difficulty: "Hard",
+                  time: "3:42",
+                  tags: ["Rock", "Alternative", "2000s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Stayin' Alive",
+                  artist: "Bee Gees",
+                  difficulty: "Hard",
+                  time: "4:45",
+                  tags: ["Disco", "Funk", "1970s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Say So",
+                  artist: "Doja Cat",
+                  difficulty: "Medium",
+                  time: "3:57",
+                  tags: ["Pop", "Funk", "2020s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Dance Monkey",
+                  artist: "Tones and I",
+                  difficulty: "Medium",
+                  time: "3:29",
+                  tags: ["Pop", "Indie", "2010s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "September",
+                  artist: "Earth, Wind & Fire",
+                  difficulty: "Hard",
+                  time: "3:35",
+                  tags: ["Funk", "Disco", "1970s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Peaches",
+                  artist: "Justin Bieber",
+                  difficulty: "Easy",
+                  time: "3:18",
+                  tags: ["Pop", "R&B", "2020s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Let’s Groove",
+                  artist: "Earth, Wind & Fire",
+                  difficulty: "Medium",
+                  time: "5:36",
+                  tags: ["Funk", "Disco", "1980s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Waka Waka (This Time for Africa)",
+                  artist: "Shakira",
+                  difficulty: "Medium",
+                  time: "3:22",
+                  tags: ["Pop", "World", "2010s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "abcdefu",
+                  artist: "GAYLE",
+                  difficulty: "Medium",
+                  time: "2:49",
+                  tags: ["Pop", "Alternative", "2020s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Gimme! Gimme! Gimme! (A Man After Midnight)",
+                  artist: "ABBA",
+                  difficulty: "Hard",
+                  time: "4:50",
+                  tags: ["Disco", "Pop", "1970s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "Savage Love",
+                  artist: "Jason Derulo & Jawsh 685",
+                  difficulty: "Easy",
+                  time: "2:51",
+                  tags: ["Pop", "Dancehall", "2020s"],
+                  img: "./DancingDude.png"
+                },
+                {
+                  title: "I Wanna Dance with Somebody",
+                  artist: "Whitney Houston",
+                  difficulty: "Hard",
+                  time: "4:52",
+                  tags: ["Pop", "Dance", "1980s"],
+                  img: "./DancingDude.png"
+                }
+      ];
+
+
+
+export default dances;
+
